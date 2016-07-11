8456cfebd100ee5b645728f82990980a2af617e0
Ramesh Shihora <rameshshihora@fb.com>
2016-07-11 17:40:24 +0000

    MongoDb Installation

---
+++ recipes/install_mongodb.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: .
+# Recipe:: install_mongodb
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# add the MonogDB repository
+yum_repository 'mongodb-org-3.2' do
+  description "MongoDB Stable repo"
+  baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/"
+  gpgcheck true
+  enabled true
+  gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
+  action :create
+end
+
+package 'mongodb-org'
+
+service 'mongod' do 
+  action [ :enable, :start ]
+end
